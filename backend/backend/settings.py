de3550c8e1d97ac1c76ed4d50a95b190d3909683
Aftab073 <tamboliaftab84@gmail.com>
2025-04-24 16:08:44 +0530

    fixing issues

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  
     'corsheaders',  
-    'backend.search',  
+    'backend.search.apps.SearchConfig',  
 ]
 
 MIDDLEWARE = [
