fa1fd6c3d9e1a0195f8d78722e34beb5fc5e6cf1
Aftab073 <tamboliaftab84@gmail.com>
2025-04-25 20:47:58 +0530

    update requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -105,4 +105,13 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
-    X_FRAME_OPTIONS = 'DENY' 
+    X_FRAME_OPTIONS = 'DENY'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
