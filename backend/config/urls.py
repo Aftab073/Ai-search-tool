cc9ddc06d5fb3f62077da759af86d9531c75ab27
Aftab073 <tamboliaftab84@gmail.com>
2025-04-26 00:30:15 +0530

    add urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/search/', include('search.urls')),
+    path('api/search/', include('backend.search.urls')),
 ]
